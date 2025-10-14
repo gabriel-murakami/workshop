7cac9319f71e73849ce7bc1f51b104397fd9d68b
Gabriel Murakami <gabriel.m.alves42@gmail.com>
2025-10-14 00:08:37 -0300

    test

--- spec/mailers/service_order_mailer_spec.rb
+++ spec/mailers/service_order_mailer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe ServiceOrderMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Atualização da Ordem de Serviço")
       expect(mail.to).to eq([ customer.email ])
-      expect(mail.from).to eq([ ENV["SMTP_USERNAME"] ])
+      expect(mail.from).to eq(ENV["SMTP_USERNAME"])
     end
 
     it "renders the body with order info" do
